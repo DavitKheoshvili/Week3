7a4fb9fe282141ca8badc56350870af8cdf30991
dato <kheoshvili.dato@gmail.com>
2022-05-29 14:42:27 +0400

    Created html form. culr using user input for searching data

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Movies</title>
+</head>
+
+<body>
+    <form action="/index.php" method="POST">
+        <label for="name">Movie Name:</label><br>
+        <input type="text" name="name" ><br>
+        <input type="submit" value="Submit">
+    </form>
+</html>
+
 <?php
 require_once("remote/remote.php");
 
